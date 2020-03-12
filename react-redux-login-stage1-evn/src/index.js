b09e6188ca80f8f90ebfb8649c3a79f10e8da093
CornerYoung <947498533@qq.com>
2020-03-12 18:17:59 +0800

    搭建前端reducer目录文件结构

--- react-redux-login-stage1-evn/src/index.js
+++ react-redux-login-stage1-evn/src/index.js
@@ -2,4 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import {createStore,applyMiddleware} from 'redux'
+import {Provider} from 'react-redux'
+import logger from 'redux-logger'
+import thunk from 'redux-thunk'
+import {composeWithDevTools} from 'redux-devtools-extension'
+import rootReducer from './reducers'
+
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(logger, thunk)))
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+, document.getElementById('root'));
