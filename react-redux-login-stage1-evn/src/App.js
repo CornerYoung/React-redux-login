c2f9b63e252011c4a9d812b9512acfde5f842528
CornerYoung <947498533@qq.com>
2020-03-12 18:26:15 +0800

    引入bootstrap压缩样式文件，优化界面效果

--- react-redux-login-stage1-evn/src/App.js
+++ react-redux-login-stage1-evn/src/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      App
+    <div className="jumbotron">
+      <h1>Hello React!</h1>
     </div>
   );
 }
--- react-redux-login-stage1-evn/src/reducers/auth.js
+++ react-redux-login-stage1-evn/src/reducers/auth.js
@@ -1,6 +1,8 @@
-export default function(state={},action){
-    switch(action){
+const auth = (state={},action) => {
+    switch(action.type){
         default:
             return state
     }
 }
+
+export default auth
