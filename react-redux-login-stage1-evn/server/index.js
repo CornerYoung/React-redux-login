6caed7eaeb066aa818d905d815dba4a8fe5fdc4a
CornerYoung <947498533@qq.com>
2020-03-13 10:23:46 +0800

    修改入口文件响应

--- react-redux-login-stage1-evn/server/index.js
+++ react-redux-login-stage1-evn/server/index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const user = require('./routes/user')
+const debug = require('debug')('my-application')
 const app = express()
 
 app.use('/api/user',user)
 
 app.listen(3001,(req,res)=>{
-    console.log('服务器运行在3001端口上')
+    debug('服务器运行在3001端口上')
 })
