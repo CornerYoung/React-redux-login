e1a3f45d3369096c711ca6f5a7a486557dd0bad5
CornerYoung <947498533@qq.com>
2020-03-16 14:27:09 +0800

    后端验证用户填写信息正确性

--- react-redux-login-stage1-evn/server/routes/user.js
+++ react-redux-login-stage1-evn/server/routes/user.js
@@ -1,10 +1,37 @@
 const express = require('express')
 const router = express.Router()
+const isEmpty = require('loadsh/isEmpty')
+const validator = require('validator')
 
-router.get('/', (req, res) => {
-    res.send({
-        msg: 'Hello Node!!!'
-    })
+const validatorInput = (data)=>{
+    let errors = {}
+    if(validator.isEmpty(data.username)){
+        errors.username = '请输入用户名'
+    }
+    if(validator.isEmail(data.email)){
+        errors.email = '请填写邮箱'
+    }
+    if(validator.isEmpty(data.password)){
+        errors.password = '请填写密码'
+    }
+    if(validator.isEmpty(data.passwordConfirmation)) {
+        errors.passwordConfirmation = '请确认密码'
+    }
+    if(validator.equals(data.password,data.passwordConfirmation)){
+        errors.passwordConfirmation = '两次填写密码不一致'
+    }
+    return {
+        errors,
+        isValid:isEmpty(errors)
+    }
+}
+
+router.post('/', (req, res) => {
+    console.log(req.body)
+    const {errors,isValid} = validatorInput(req.body)
+    if(!isValid){
+        res.status(400).json(errors)
+    }
 })
 
 module.exports = router
