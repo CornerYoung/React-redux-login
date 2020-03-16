567813a33b857c5b0f12b85953594d01f032c792
CornerYoung <947498533@qq.com>
2020-03-16 14:25:12 +0800

    路径错误修复

--- react-redux-login-stage1-evn/server/index.js
+++ react-redux-login-stage1-evn/server/index.js
@@ -2,8 +2,10 @@ const express = require('express')
 const user = require('./routes/user')
 const debug = require('debug')('my-application')
 const app = express()
+const bodyParser = require('body-parser')
 
-app.use('/api/user',user)
+app.use(bodyParser.json())
+app.use('/api/users',user)
 
 app.listen(3001,(req,res)=>{
     debug('服务器运行在3001端口上')
